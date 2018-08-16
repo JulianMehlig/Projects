0de82138a089582aef85e22787ef13a8ec579cae
Julian Mehlig <32087968+JulianMehlig@users.noreply.github.com>
2018-08-16 10:13:41 +0200

    14082018-FinalVersion1.0

--- NavDrawer/app/src/main/java/com/julianmehlig/navdrawer/MainActivity.java
+++ NavDrawer/app/src/main/java/com/julianmehlig/navdrawer/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
